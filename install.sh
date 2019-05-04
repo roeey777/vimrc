ee96a123b7302562bb7b37e957e40236d3552dd8
Eyal Royee <eyalroyee@gmail.com>
2019-05-04 21:39:57 +0300

    added YouCompleteMe plugin.
    Signed-off-by: Eyal Royee <eyalroyee@gmail.com>

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #! /bin/sh
 
+# install all the submodules (plugins)
+git submodule update --init --recursive
+
+# compile YouCompleteMe
+pushd bundle/YouCompeleteMe/
+python install.py --clang-completer
+popd
+
 VIM_CONFIG_DIR=$(pwd)
 
 ln -snf ${VIM_CONFIG_DIR}/vimrc.vim ${HOME}/.vimrc
